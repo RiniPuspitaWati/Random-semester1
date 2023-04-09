8fa9161ffc5b9d18ef8d18ee6350000c977ef709
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-04-09 19:14:21 +0700

    Add files via upload

---
+++ Random semseter1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<windows.h>
+#include<curses.h>
+using namespace std;
+int main ()
+{ 
+/*int a;
+printf("Nilai Integer: ");
+scanf("%d",&a);
+a-=a++;
+printf("\na-=a++ => %d",a);
+*/
+int i=2;
+printf("%d\n",8*i/3);
+printf("%d\n",8.0*i);
+printf("%f\n",8*i);
+printf("%f\n",8*i/3);
+printf("%d\n",8*i%3);
+printf("%f\n",8.0*i/3);
+return 0;
+}
